5163b0b9356ee540c5807eea5249913ec9ab9dbf
Dimon <Dimon999>
2023-09-20 22:34:43 +0300

    Решена 3 задача

---
+++ task_1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число");
+int square = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ввелите второе число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(square == number * number) 
+{
+    Console.WriteLine("true");
+}
+else 
+{
+   Console.WriteLine("false"); 
+}
